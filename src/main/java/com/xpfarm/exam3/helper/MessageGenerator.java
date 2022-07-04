383a1ee3dff9cdf860da0b4294be56dbe4472874
David Mark Meszaros <david.mark.meszaros@ibm.com>
2022-07-04 09:06:36 +0200

    feat: message generator - won footer - GREEN

--- src/main/java/com/xpfarm/exam3/helper/MessageGenerator.java
+++ src/main/java/com/xpfarm/exam3/helper/MessageGenerator.java
@@ -15,8 +15,8 @@ public class MessageGenerator {
     public String printStepHeader(Symbol playerSymbol) {
         return "Player " + playerSymbol.getValue() + ":" + EOL;
     }
-    
+
     public String printWonFooter(Symbol playerSymbol) {
-        return null;
+        return "PLAYER " + playerSymbol.getValue() + " WON!" + EOL;
     }
 }
