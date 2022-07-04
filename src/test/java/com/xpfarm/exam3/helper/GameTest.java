2477291e3d8db0bbc8bafae297d3d36498084eee
David Mark Meszaros <david.mark.meszaros@ibm.com>
2022-07-04 11:19:27 +0200

    feat: game - getState - RED

--- src/test/java/com/xpfarm/exam3/helper/GameTest.java
+++ src/test/java/com/xpfarm/exam3/helper/GameTest.java
@@ -44,4 +44,23 @@ class GameTest {
         // then
         assertThat(actualResult).isEqualTo(expectedResult);
     }
+
+    @Test
+    @DisplayName("GIVEN X won WHEN getState THEN should return GAME_OVER")
+    void shouldReturnGameOver2() {
+        // given
+        Game game = new Game();
+        GameState expectedResult = GameState.GAME_OVER;
+        game.init();
+        game.step(0, 0);
+        game.step(1, 2);
+        game.step(0, 1);
+        game.step(1, 0);
+        game.step(0, 2);
+        // when
+        GameState actualResult = game.getState();
+
+        // then
+        assertThat(actualResult).isEqualTo(expectedResult);
+    }
 }
