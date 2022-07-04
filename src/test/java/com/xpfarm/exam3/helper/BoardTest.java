777fa6b668c8b31802209cab22c392e4eb9a5780
David Mark Meszaros <david.mark.meszaros@ibm.com>
2022-07-04 09:59:36 +0200

    feat: board - Diagonal #1 (0,4,8) - RED

--- src/test/java/com/xpfarm/exam3/helper/BoardTest.java
+++ src/test/java/com/xpfarm/exam3/helper/BoardTest.java
@@ -154,4 +154,22 @@ class BoardTest {
         // then
         assertThat(actualResult).isTrue();
     }
+
+    @Test
+    @DisplayName("GIVEN X won with [0,4,8] WHEN isGameEnded THEN should return true'")
+    void shouldReturnGameEnded7() {
+        // given
+        Board board = new Board();
+        board.mark(0, 0, Symbol.X);
+        board.mark(0, 2, Symbol.O);
+        board.mark(1, 1, Symbol.X);
+        board.mark(1, 0, Symbol.O);
+        board.mark(2, 2, Symbol.X);
+
+        // when
+        Boolean actualResult = board.isGameEnded();
+
+        // then
+        assertThat(actualResult).isTrue();
+    }
 }
