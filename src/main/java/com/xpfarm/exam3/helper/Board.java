ac33c3143b06803d7cfc9dfd36d6c73bb89d5e68
David Mark Meszaros <david.mark.meszaros@ibm.com>
2022-07-04 08:56:39 +0200

    feat: board - Horizontal #1 (0,1,2) - RED

--- src/main/java/com/xpfarm/exam3/helper/Board.java
+++ src/main/java/com/xpfarm/exam3/helper/Board.java
@@ -22,4 +22,8 @@ public class Board {
     public void mark(int x, int y, Symbol symbol) {
         symbols.set(x * 3 + y, symbol);
     }
+    
+    public Boolean isGameEnded() {
+        return false;
+    }
 }
--- src/test/java/com/xpfarm/exam3/helper/BoardTest.java
+++ src/test/java/com/xpfarm/exam3/helper/BoardTest.java
@@ -44,4 +44,22 @@ class BoardTest {
         // then
         assertThat(actualResult).isEqualTo(expectedResult);
     }
+
+    @Test
+    @DisplayName("GIVEN X won with [0,1,2] WHEN isGameEnded THEN should return true'")
+    void shouldReturnGameEnded() {
+        // given
+        Board board = new Board();
+        board.mark(0, 0, Symbol.X);
+        board.mark(1, 2, Symbol.X);
+        board.mark(0, 1, Symbol.X);
+        board.mark(1, 1, Symbol.X);
+        board.mark(0, 2, Symbol.X);
+
+        // when
+        Boolean actualResult = board.isGameEnded();
+
+        // then
+        assertThat(actualResult).isTrue();
+    }
 }
