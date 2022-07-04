38ea035af56490010caf6e58f132a93bc39b51d4
David Mark Meszaros <david.mark.meszaros@ibm.com>
2022-07-04 08:09:51 +0200

    feat: board - print after mark - GREEN

--- src/main/java/com/xpfarm/exam3/helper/Board.java
+++ src/main/java/com/xpfarm/exam3/helper/Board.java
@@ -1,18 +1,25 @@
 package com.xpfarm.exam3.helper;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Board {
     static final String EOL = System.lineSeparator();
-    static final String EMPTY_LINE = " | | ";
+
+    List<Symbol> symbols = new ArrayList<>(
+            List.of(Symbol.EMPTY, Symbol.EMPTY, Symbol.EMPTY,
+                    Symbol.EMPTY, Symbol.EMPTY, Symbol.EMPTY,
+                    Symbol.EMPTY, Symbol.EMPTY, Symbol.EMPTY));
 
     public String print() {
-        return EMPTY_LINE + EOL
+        return symbols.get(0).getValue() + "|" + symbols.get(1).getValue() + "|" + symbols.get(2).getValue() + EOL
                 + "-+-+-" + EOL
-                + EMPTY_LINE + EOL
+                + symbols.get(3).getValue() + "|" + symbols.get(4).getValue() + "|" + symbols.get(5).getValue() + EOL
                 + "-+-+-" + EOL
-                + EMPTY_LINE + EOL;
+                + symbols.get(6).getValue() + "|" + symbols.get(7).getValue() + "|" + symbols.get(8).getValue() + EOL;
     }
 
     public void mark(int x, int y, Symbol symbol) {
-        
+        symbols.set(x * 3 + y, symbol);
     }
 }
--- src/main/java/com/xpfarm/exam3/helper/Symbol.java
+++ src/main/java/com/xpfarm/exam3/helper/Symbol.java
@@ -1,5 +1,15 @@
 package com.xpfarm.exam3.helper;
 
 public enum Symbol {
-    EMPTY, X, O
+    EMPTY(" "), X("X"), O("O");
+
+    private final String value;
+
+    Symbol(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
 }
